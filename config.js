0ed7e67b59152b31f6bffb60dc217a050d6e76e2
zxLau <657413201@qq.com>
2019-12-19 16:37:38 +0800

    init notes

---
+++ config.js
@@ -0,0 +1,22 @@
+var CONFIG = {
+  // 网页 title
+  title: '笔记',
+  
+  // 网页 keywords
+  keywords: 'notes',
+
+  // 网页 description
+  description: 'notes',
+
+  // 链接是否打开新的窗口
+  openNewWindow: true,
+
+  // 页面右上角的 github 链接
+  github: '',
+
+  // 左侧目录的md文件
+  summaryMd: 'SUMMARY.md',
+
+  // 网页默认访问的md文件
+  index: 'README.md'
+}
